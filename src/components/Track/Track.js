1c8b3d7a515c60b27f1a070fdff30ee43f74f210
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-06-05 12:41:32 +0100

    Update Track.js

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import './Track.css'
+import React from "react";
+import "./Track.css";
 
 function Track(props) {
-    return (
-        <div className="Track">
-            <div className="Track-information">
-                <h3>{props.track.title}</h3>
-                <p>{props.track.artist} | {props.track.album}</p>
-            </div>
-            <a className="Track-action" 
-            onClick={() => props.handleTrackAction(props.track)}>
-                {props.trackActionCharacter}
-            </a>
-        </div>
-    )
+  return (
+    <div className="Track">
+      <div className="Track-information">
+        <h3>{props.track.title}</h3>
+        <p>
+          {props.track.artist} | {props.track.album}
+        </p>
+      </div>
+      <a
+        className="Track-action"
+        onClick={() => props.handleTrackAction(props.track)}
+      >
+        {props.trackActionCharacter}
+      </a>
+    </div>
+  );
 }
 
 export default Track;
